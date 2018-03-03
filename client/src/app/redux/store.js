29d8611801b9c3d2f4ccf743034e2c82e62965f7
FXG387 <anton.myshenin@motorolasolutions.com>
2018-03-03 13:06:59 +0100

    ready tabs

--- client/src/app/redux/store.js
+++ client/src/app/redux/store.js
@@ -2,9 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import location from './reducers/location';
+import tabs from "./reducers/tabs";
 
 export default createStore(
-    combineReducers({location}),
+    combineReducers({location, tabs}),
     {},
     applyMiddleware(createLogger(), thunk),
 );
