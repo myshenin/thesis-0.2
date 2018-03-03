5852e7efee968760f90ab866bc0d213090ba6d73
FXG387 <anton.myshenin@motorolasolutions.com>
2018-03-03 21:51:27 +0100

    cors errors

--- client/src/app/redux/reducers/weather/actions/getData.js
+++ client/src/app/redux/reducers/weather/actions/getData.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const getData = (payload) => {
     return {
         type: 'GET_DATA',
-        payload: axios.post('https://gpcsw6awq9.execute-api.eu-central-1.amazonaws.com/prod/', payload)
+        payload: axios.post('https://efbfsoii0d.execute-api.eu-central-1.amazonaws.com/prod/weather', payload)
     };
 };
 
